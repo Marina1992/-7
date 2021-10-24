be08f3cf4e8798b3e5a9c808401dbc1fa188cf97
Марина Разумова <marina.razumova.19@yandex.ru>
2021-10-24 19:29:42 +0300

    Добавьте файлы проекта.

---
+++ Задача1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { /*  Два треугольника заданы длинами своих сторон. 
+           *  Определить, площадь какого из них больше (создать метод для вычисления  площади  треугольника по длинам его сторон).  
+           *  Для решения задачи можно использовать формулу Герона   */
+            
+          
+       
+            
+             double S= GetParam(8, 8, 8 );
+            Console.WriteLine("Площадь 1 треугольника {0:f2}" ,S);
+
+            double S1 = GetParam(15, 20, 10);
+            Console.WriteLine("Площадь 2 треугольника {0:f2}", S1);
+
+            if (S>S1)
+            { Console.WriteLine("Площадь первого больше"); }
+
+            else
+            {
+                { Console.WriteLine("Площадь второго  больше"); }
+            }
+            Console.ReadKey();
+
+        }
+
+
+        static double GetParam(int x, int y,  int z )
+        {
+            
+           double P = (x + y + z) /2;
+           double S =  (Math.Sqrt(P*(P-x)* (P - y) * (P - z))) ;
+            return S;
+
+        }
+    }
+}
---
+++ Задача2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Создать метод для вычисления объема и площади поверхности куба по длине его ребра.  */
+
+            Console.WriteLine("Введите сторону куба а:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            
+            double V, S;
+            GetParam(a,  out V, out S);
+            Console.WriteLine(" Объем куба :{0}",V);
+            Console.WriteLine(" Площадь поверхности   куба :{0}", S);
+            Console.ReadKey();
+
+        }
+
+
+        static void GetParam(int a,  out double V, out double S)
+        {
+            S = 6 * a * a;
+            V =S*a;
+           
+        }
+
+    }
+}
+
